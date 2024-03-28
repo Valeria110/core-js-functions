89b382b1f45e23abb3e3e128e87e448cd8255987
Valeriya Ivashkevich <ivashkevich03@gmail.com>
2024-03-28 21:21:32 +0300

    feat: add task solutions

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -153,8 +153,19 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return () => {
+    for (let i = 1; i <= attempts; i += 1) {
+      try {
+        return func();
+      } catch (err) {
+        if (i === attempts) {
+          throw new Error('Error is: ', err);
+        }
+      }
+    }
+    return null;
+  };
 }
 
 /**
